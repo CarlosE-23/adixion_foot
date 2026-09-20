4d1e544fa16457ad85086b396d933b79519779b0
Carlos Guanipa <carlosguanipa23@gmail.com>
2026-09-19 20:45:49 -0400

    correccion de error en components/Navbar/index.ts

--- components/Navbar/index.ts
+++ components/Navbar/index.ts
@@ -1,4 +1,9 @@
-export type { NavItem, NavbarLogo, NavbarScrollState, NavbarContext } from "./types";
+export type {
+    NavItem,
+    NavbarLogo,
+    NavbarScrollState,
+    NavbarContext,
+} from "./types";
 export type { NavbarProps } from "./Navbar.types";
-export { navItems, logo } from "./navItems";
+export { navItems, logo } from "./Navbar.data";
 export { Navbar } from "./Navbar";
